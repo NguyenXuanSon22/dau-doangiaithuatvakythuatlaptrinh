e23ca2d208ca8a86cc6b1fa7a165e80932a6426d
NguyenXuanSon22 <son_2051220077@dau.edu.vn>
2026-05-13 20:33:53 +0700

    Create Most Frequent Number Following Key In an Array.cpp

---
+++ Most Frequent Number Following Key In an Array.cpp
@@ -0,0 +1,9 @@
+class Solution(object):
+    def mostFrequent(self, nums, key):
+        count = defaultdict(int)
+        
+        for i in range(len(nums) - 1):
+            if nums[i] == key:
+                count[nums[i + 1]] += 1
+        return max(count, key=lambda x: count[x])
+        
