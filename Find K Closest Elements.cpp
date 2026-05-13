272f40439f75bd73950e7c76c484a9a6b0070dc8
NguyenXuanSon22 <son_2051220077@dau.edu.vn>
2026-05-13 20:24:58 +0700

    Create Find K Closest Elements.cpp

---
+++ Find K Closest Elements.cpp
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findClosestElements(self, arr, k, x):
+        left, right = 0, len(arr) - k
+        while left < right:
+            mid = left + (right - left) // 2
+            if x - arr[mid] > arr[mid + k] - x:
+                left = mid + 1
+            else:
+                right = mid
+        return arr[left:left + k]
+        
