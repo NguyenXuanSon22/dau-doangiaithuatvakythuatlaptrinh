100d001eff81e60ef0061e60dc69bd912e25f467
NguyenXuanSon22 <son_2051220077@dau.edu.vn>
2026-05-13 21:07:39 +0700

    Create Find All Numbers Disappeared in an Array.cpp

---
+++ Find All Numbers Disappeared in an Array.cpp
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        for num in nums:
+            index = abs(num) - 1  
+            nums[index] = -abs(nums[index])  
+    
+        result = []
+        for i, num in enumerate(nums):
+            if num > 0:
+                result.append(i + 1)      
+        return result
+        
