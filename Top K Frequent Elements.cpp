059087bcd982645601b407f286e74cacb873df06
NguyenXuanSon22 <son_2051220077@dau.edu.vn>
2026-05-13 21:15:34 +0700

    Create Top K Frequent Elements.cpp

---
+++ Top K Frequent Elements.cpp
@@ -0,0 +1,13 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        freq = Counter(nums)
+        bucket = [[] for _ in range(len(nums) + 1)]
+        for num, count in freq.items():
+            bucket[count].append(num)
+        res = []
+        for i in range(len(bucket) - 1, 0, -1):
+            for num in bucket[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+        
