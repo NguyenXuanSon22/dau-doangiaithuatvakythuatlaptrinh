86182789e332ce7d682d7e394d874d71e3c7726d
NguyenXuanSon22 <son_2051220077@dau.edu.vn>
2026-05-13 19:55:25 +0700

    Create Three Divisors.cpp

---
+++ Three Divisors.cpp
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isThree(self, n):
+        root = int(n ** 0.5) 
+        if root * root != n:
+            return False  
+        if root < 2:
+            return False
+        for i in range(2, int(root ** 0.5) + 1):
+            if root % i == 0:
+                return False
+        return True
+        
