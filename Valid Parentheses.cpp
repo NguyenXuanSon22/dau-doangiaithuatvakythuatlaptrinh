a121b27865bd10fd332626a0a712fc3bd35ffad4
NguyenXuanSon22 <son_2051220077@dau.edu.vn>
2026-05-13 20:27:21 +0700

    Create Valid Parentheses.cpp

---
+++ Valid Parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in bracket_map: 
+                if stack and stack[-1] == bracket_map[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:  
+                stack.append(char)
+        
+        return len(stack) == 0
+        
