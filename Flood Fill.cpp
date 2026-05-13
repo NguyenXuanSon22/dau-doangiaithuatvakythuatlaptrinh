29c145f852137424bb33245b9f8a9fa71a51277e
NguyenXuanSon22 <son_2051220077@dau.edu.vn>
2026-05-13 20:04:33 +0700

    Create Flood Fill.cpp

---
+++ Flood Fill.cpp
@@ -0,0 +1,18 @@
+class Solution(object):
+    def floodFill(self, image, sr, sc, color):
+        original_color = image[sr][sc]
+        if original_color == color:
+            return image 
+        rows, cols = len(image), len(image[0])
+        def dfs(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols or image[r][c] != original_color:
+                return
+            image[r][c] = color
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        dfs(sr, sc)
+        return image
+        
