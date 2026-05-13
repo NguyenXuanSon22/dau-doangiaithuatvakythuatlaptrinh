380d04c1330a9a9c632554733f0ae52ddf5ebb99
NguyenXuanSon22 <son_2051220077@dau.edu.vn>
2026-05-13 20:35:35 +0700

    Create Minimum Sum of Four Digit Number After Splitting Digits.cpp

---
+++ Minimum Sum of Four Digit Number After Splitting Digits.cpp
@@ -0,0 +1,9 @@
+class Solution(object):
+    def minimumSum(self, num):
+        digits = sorted(str(num))
+        
+        new1 = int(digits[0] + digits[2])
+        new2 = int(digits[1] + digits[3])
+        
+        return new1 + new2
+        
